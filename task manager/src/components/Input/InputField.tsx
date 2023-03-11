1c1f6d6788a76326cbaa34ffe3a7193f22936a93
Caio Theodoro de Carvalho <caio.theodoroc@gmail.com>
2023-03-11 01:28:10 -0300

    finishing logic of adding task and taks interface for iteration and manipulation

--- task manager/src/components/Input/InputField.tsx
+++ task manager/src/components/Input/InputField.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 import styles from './InputField.module.css';
 function InputField(props: insertTaskInput){
-    const {placeHolder,type} = props;
+    const {placeHolder,type,onChange} = props;
     return (
         <input
          type={type}
          className={styles.insertTaskInput}
          placeholder={placeHolder}
-         
+         onChange={onChange}
         />
     )
 }
-interface insertTaskInput{
+interface insertTaskInput extends InputHTMLAttributes<HTMLInputElement>{
     placeHolder?:string,
     type:string,
 }
--- task manager/src/components/WorkBench/WorkBench.tsx
+++ task manager/src/components/WorkBench/WorkBench.tsx
@@ -1,15 +1,42 @@
+import { useState,ChangeEvent, useEffect } from 'react';
 import styles from './WorkBench.module.css';
 import InputField from '../Input/InputField';
 import Button from '../Button/Button';
 
-
+interface Task{
+    index:number,
+    taskDescription:string,
+    isCompleted:Boolean
+}
 function WorkBench(){
+    const [newInputedTaskValue,setNewInputedTaskValue] = useState('');
+    const [tasks,setTasks] = useState<Array<Task>>([]);
 
+
+
+    const onChange = (event:ChangeEvent<HTMLInputElement>) =>{
+        setNewInputedTaskValue(event.target.value);
+    };
+
+    useEffect(()=>{console.log("!@# taks atualizadas ",tasks,newInputedTaskValue)},[tasks,newInputedTaskValue]);
     return(
         <main className={styles.workBenchContainer}>      
         <section className={styles.taskRegisterContainer}>  
-            <InputField placeHolder='Adicione uma nova tarefa' type='text'/>
-            <Button createButton text='Criar' onClick={()=>console.log("!@# the button was clicked")}/>
+            <InputField placeHolder='Adicione uma nova tarefa' type='text' onChange={onChange}/>
+            <Button createButton text='Criar' onClick={()=>{
+                
+                const newTask :Task = 
+                {index:0,taskDescription:newInputedTaskValue,isCompleted:false};
+                if(!tasks.length)
+                    setTasks([...tasks,newTask])
+                else{                    
+                    let lastTask =  tasks[tasks.length-1];
+                    
+                    if(lastTask)
+                    newTask.index = lastTask.index + 1;
+                    setTasks([...tasks,newTask]);
+                }
+            }}/>
         </section>
 
         </main>
